6e3c2830e105ab525f3ca285e04ee797022458af
foued <foued@gmail.com>
2022-01-17 18:20:15 +0100

    end off video 6

--- src/App.js
+++ src/App.js
@@ -8,20 +8,25 @@ import Activate from "./containers/Activate";
 import ResetPassword from "./containers/ResetPassword";
 import ResetPasswordConfirm from "./containers/ResetPasswordConfirm";
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => (
-  <Router>
-    <Layout>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/signup" element={<Signup/>}/> 
-        
-        <Route exact path='/reset-password' element={<ResetPassword/>}/>
-        <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm/>}/>
-        <Route exact path='/activate/:uid/:token' element={<Activate/>}/>
-      </Routes>
-    </Layout>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/signup" element={<Signup/>}/> 
+          
+          <Route exact path='/reset-password' element={<ResetPassword/>}/>
+          <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm/>}/>
+          <Route exact path='/activate/:uid/:token' element={<Activate/>}/>
+        </Routes>
+      </Layout>
+    </Router>
+  </Provider>
 );
 
 export default App;
